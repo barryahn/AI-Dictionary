d3d35eba155dee28fe71bbb71f7e7d976f2faf05
Ahn ByeongHyeon <themechanic002@gmail.com>
2025-09-03 12:35:49 +0900

    Add: keystore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
@@ -33,11 +42,25 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") { // "release"라는 이름으로 signingConfig를 생성
+            storeFile = if (keystoreProperties.containsKey("storeFile")) file(keystoreProperties["storeFile"].toString()) else null
+            storePassword = keystoreProperties["storePassword"]?.toString()
+            keyAlias = keystoreProperties["keyAlias"]?.toString()
+            keyPassword = keystoreProperties["keyPassword"]?.toString()
+        }
+    }
+
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") { // "release" buildType을 가져옴
+            signingConfig = signingConfigs.getByName("release") // 위에서 정의한 signingConfig를 연결
+
+            //코드난독화 및 사이즈 축소
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
