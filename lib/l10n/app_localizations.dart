4a757bdbfedfa2cbf2f8c12b867c2565c986b620
Ahn ByeongHyeon <themechanic002@gmail.com>
2025-09-09 14:55:46 +0900

    Version update

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -12,7 +12,7 @@ class AppLocalizations {
 
   // 앱 이름 상수
   static const String appName = 'WordVibe';
-  static const String appVersion = '1.0.1';
+  static const String appVersion = '1.0.3';
 
   static const Map<String, Map<String, String>> _localizedValues = {
     'ko': {
